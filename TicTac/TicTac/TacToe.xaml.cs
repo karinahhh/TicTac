ccccd2d01e4fafe5e7efb1b5cd60c32c7c2cc04a
opilane <opilane@HP-CZC6297BB1>
2020-11-05 11:14:14 +0200

    Add project files.

---
+++ TicTac/TicTac/TacToe.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TicTac
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TacToe : ContentPage
+	{
+		Label[,] tic = new Label[3, 3];
+		int stps, chck;
+		string l;
+		Label lbl;
+		Button change, restart, own;
+		public TacToe()
+		{
+			Grid();
+			stps = 0;
+			own.IsEnabled = true;
+		}
+
+		void Grid()
+		{
+			stps = 0;
+			Grid grd = new Grid();
+			for (int i = 0; i < 3; i++)
+			{
+				grd.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+			}
+			for (int w = 0; w < 3; w++)
+			{
+				grd.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+			}
+			//1BUTTON
+			change = new Button();
+			change.Text = "Случайный выбор игрока";
+			grd.Children.Add(change,0,3);
+
+			//2BUTTON
+			restart = new Button();
+			restart.Text = "Начать заново";
+			grd.Children.Add(restart, 1, 3);
+			
+			//3BUTTON
+			own = new Button();
+			own.Text = "Выбрать первого игрока";
+			grd.Children.Add(own, 2, 3);
+
+			change.Clicked += Change_Clicked;
+			restart.Clicked += Restart_Clicked;
+			own.Clicked += Own_Clicked;
+
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					lbl = new Label
+					{
+						BackgroundColor = Color.FromRgb(191, 155, 178),
+						FontSize = 100,
+						Text = "",
+						HorizontalTextAlignment = TextAlignment.Center,
+						TextColor = Color.FromRgb(151, 135, 194),
+						VerticalTextAlignment = TextAlignment.Center,
+					};
+					tic[i, j] = lbl;
+					l = "X";
+					var tap = new TapGestureRecognizer();
+					tap.Tapped += Tap_Tapped;
+					grd.Children.Add(lbl, i, j);
+					lbl.GestureRecognizers.Add(tap);
+				}
+			}
+
+			Content = grd;
+		}
+		//CLICKED START
+		private void Tap_Tapped(object sender, EventArgs e)
+		{/*
+			Label label = sender as Label;
+			if (chck % 2 == 0)
+			{
+				change.Text = "0";
+				label.Text = l;
+				chck++;
+				stps++;
+			}
+			else if (chck % 2 != 0)
+			{
+				change.Text = "X";
+				chck++;
+				stps++;
+				label.Text = "0";
+			}
+			if (check() == true)
+			{
+				DisplayAlert("Игра окончена!", "Ничья", "Новая игра");
+				Grid();
+				stps = 0;
+			}
+
+			else if (checkY() == true)
+			{
+				DisplayAlert("Игра окончена!", win, "Новая игра");
+				Grid();
+			}
+			else if (checkX() == true)
+			{
+				DisplayAlert("Игра окончена!", win, "Новая игра");
+				Grid();
+			}
+			else if (checkXY() == true)
+			{
+				DisplayAlert("Игра окончена!", win, "Новая игра");
+				Grid();
+			}*/
+		}
+
+		private void Own_Clicked(object sender, EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void Restart_Clicked(object sender, EventArgs e)
+		{
+			Grid();
+			chck = 0;
+			stps = 0;
+		}
+
+		private void Change_Clicked(object sender, EventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		//CLICKED END
+	}
+}
---
+++ TicTac/TicTac/Tic.xaml.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TicTac
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Tic : ContentPage
+	{
+            Label[,] tic = new Label[3, 3];
+            string l;
+            Label lbl;
+            Button change, restart, man;
+        public Tic()
+        {
+                Grid();
+                stps = 0;
+                man.IsEnabled = true;
+            }
+            //InitializeComponent();
+            void Grid()
+            {
+                stps = 0;
+                Grid grid = new Grid();
+                for (int h = 0; h < 3; h++)
+                {
+
+                    grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                }
+                for (int w = 0; w < 3; w++)
+                {
+                    grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                }
+                change = new Button
+                {
+                    Text = "Случайный выбор игрока"
+                };
+                change.Clicked += Change_Clicked;
+                grid.Children.Add(change, 0, 3);
+
+                restart = new Button
+                {
+                    Text = "Начать заново"
+                };
+                restart.Clicked += Restart_Clicked;
+                grid.Children.Add(restart, 1, 3);
+
+                man = new Button
+                {
+                    Text = "Выбрать первого игрока"
+                };
+                man.Clicked += Man_Clicked;
+                grid.Children.Add(man, 2, 3);
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        lbl = new Label
+                        {
+                            BackgroundColor = Color.FromRgb(191, 155, 178),
+                            FontSize = 100,
+                            Text = "",
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            TextColor = Color.FromRgb(151, 135, 194),
+                            VerticalTextAlignment = TextAlignment.Center,
+                        };
+                        tic[i, j] = lbl;
+                        l = "X";
+                        var tap = new TapGestureRecognizer();
+                        tap.Tapped += Tap_Tapped;
+                        grid.Children.Add(lbl, i, j);
+                        lbl.GestureRecognizers.Add(tap);
+                    }
+                }
+
+                Content = grid;
+            }
+
+            private void Restart_Clicked(object sender, EventArgs e)
+            {
+                Grid();
+                chck = 0;
+                stps = 0;
+            }
+
+            private async void Man_Clicked(object sender, EventArgs e)
+            {
+                string choice = await DisplayActionSheet("Кто начинает?", "X", "0", "Выбирайте знак");
+
+                if (choice == "X")
+                {
+                    chck = 2;
+                    change.Text = "X";
+                    man.IsEnabled = false;
+
+                }
+                else if (choice == "0")
+                {
+
+                    chck = 1;
+                    change.Text = "0";
+                    man.IsEnabled = false;
+
+                }
+
+                else
+                {
+                    man.IsEnabled = true;
+                }
+            }
+            private void Tap_Tapped(object sender, EventArgs e)
+
+            {
+                Label label = sender as Label;
+                if (chck % 2 == 0)
+                {
+                    change.Text = "0";
+                    label.Text = l;
+                    chck++;
+                    stps++;
+                }
+                else if (chck % 2 != 0)
+                {
+                    change.Text = "X";
+                    chck++;
+                    stps++;
+                    label.Text = "0";
+                }
+                if (check() == true)
+                {
+                    DisplayAlert("Игра окончена!", "Ничья", "Новая игра");
+                    Grid();
+                    stps = 0;
+                }
+
+                else if (checkY() == true)
+                {
+                    DisplayAlert("Игра окончена!", win, "Новая игра");
+                    Grid();
+                }
+                else if (checkX() == true)
+                {
+                    DisplayAlert("Игра окончена!", win, "Новая игра");
+                    Grid();
+                }
+                else if (checkXY() == true)
+                {
+                    DisplayAlert("Игра окончена!", win, "Новая игра");
+                    Grid();
+                }
+            }
+            bool check() //Ничья
+            {
+                if (stps == 9)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            string win = "";
+            bool checkY() //Победа по вертикали
+            {
+
+                if (tic[0, 0].Text == "X" && tic[0, 1].Text == "X" && tic[0, 2].Text == "X")
+                {
+                    win = "Победитель X";
+                    return true;
+                }
+
+                else if (tic[1, 0].Text == "X" && tic[1, 1].Text == "X" && tic[1, 2].Text == "X")
+                {
+                    win = "Победитель X";
+                    return true;
+                }
+
+                else if (tic[2, 0].Text == "X" && tic[2, 1].Text == "X" && tic[2, 2].Text == "X")
+                {
+                    win = "Победитель X";
+                    return true;
+                }
+
+                else if (tic[0, 0].Text == "0" && tic[0, 1].Text == "0" && tic[0, 2].Text == "0")
+                {
+                    win = "Победитель 0";
+                    return true;
+                }
+
+                else if (tic[1, 0].Text == "0" && tic[1, 1].Text == "0" && tic[1, 2].Text == "0")
+                {
+                    win = "Победитель 0";
+                    return true;
+                }
+
+                else if (tic[2, 0].Text == "0" && tic[2, 1].Text == "X" && tic[2, 2].Text == "0")
+                {
+                    win = "Победитель 0";
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            bool checkX() //Победа по горизонтали
+            {
+                if (tic[0, 0].Text == "X" && tic[1, 0].Text == "X" && tic[2, 0].Text == "X")
+                {
+                    win = "Победитель X";
+                    return true;
+                }
+                else if (tic[0, 1].Text == "X" && tic[1, 1].Text == "X" && tic[2, 1].Text == "X")
+                {
+                    win = "Победитель X";
+                    return true;
+                }
+                else if (tic[0, 2].Text == "X" && tic[1, 2].Text == "X" && tic[2, 2].Text == "X")
+                {
+                    win = "Победитель X";
+                    return true;
+                }
+                else if (tic[0, 0].Text == "0" && tic[1, 0].Text == "0" && tic[2, 0].Text == "0")
+                {
+                    win = "Победитель 0";
+                    return true;
+                }
+                else if (tic[0, 1].Text == "0" && tic[1, 1].Text == "0" && tic[2, 1].Text == "0")
+                {
+                    win = "Победитель 0";
+                    return true;
+                }
+                else if (tic[0, 2].Text == "0" && tic[1, 2].Text == "0" && tic[2, 2].Text == "0")
+                {
+                    win = "Победитель 0";
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            bool checkXY() //Победа по диагонали
+            {
+                if (tic[0, 0].Text == "X" && tic[1, 1].Text == "X" && tic[2, 2].Text == "X")
+                {
+                    win = "Победитель X";
+                    return true; ;
+                }
+                else if (tic[2, 0].Text == "X" && tic[1, 1].Text == "X" && tic[0, 2].Text == "X")
+                {
+                    win = "Победитель X";
+                    return true;
+                }
+                else if (tic[0, 0].Text == "0" && tic[1, 1].Text == "0" && tic[2, 2].Text == "0")
+                {
+                    win = "Победитель 0";
+                    return true; ;
+                }
+                else if (tic[2, 0].Text == "0" && tic[1, 1].Text == "0" && tic[0, 2].Text == "0")
+                {
+                    win = "Победитель 0";
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            int stps = 0;
+            Random rnd = new Random();
+            int chck = 0;
+            private void Change_Clicked(object sender, EventArgs e)
+            {
+                chck = rnd.Next(0, 2);
+                if (chck % 2 == 0)
+                {
+                    change.Text = "X";
+                }
+                else if (chck % 2 != 0)
+                {
+                    change.Text = "0";
+                }
+
+            }
+            /*
+			grd = new Grid()
+			{
+				HeightRequest = 400
+			};
+			for (int i = 0; i < 3; i++)
+			{
+				grd.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+				grd.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+			}*/
+
+        }
+	}
